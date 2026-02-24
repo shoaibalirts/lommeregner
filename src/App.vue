9e2dfc50fa50fb4b97a51bb09a2153e177a20ab6
shoaibalirts <shoaibali.rts@gmail.com>
2026-02-24 09:12:25 +0100

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <TheCalculator />
+  </main>
+</template>
+<script>
+import TheCalculator from "./components/TheCalculator.vue";
+
+export default {
+  data() {
+    return {
+      number1: 2,
+      number2: 4,
+    };
+  },
+  components: {
+    TheCalculator,
+  },
+};
+</script>
